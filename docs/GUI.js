7b99390d6fe4808666c08c6fa98f4d9c17c633a8
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2022-08-15 18:56:01 -0300

    Converting GUI JavaScript to classes

--- docs/GUI.js
+++ docs/GUI.js
@@ -1,54 +1,55 @@
-/* global Intl */
 import Systems from "./Amortization.js";
 
-function GUI() {
-    const formatter = new Intl.NumberFormat('pt-br', {
-        style: 'currency',
-        currency: 'BRL'
-    });
-    function cleanTable() {
+class GUI {
+    constructor() {
+        this.formatter = new Intl.NumberFormat('pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        });    
+    }
+    cleanTable() {
         let tbody = document.querySelector("tbody");
         tbody.innerHTML = "";
     }
-    function createRows(row, text) {
+    createRows(row, text) {
         let cell = document.createElement("td");
         let textNode = document.createTextNode(text);
         cell.appendChild(textNode);
         row.appendChild(cell);
     }
-    function setTotals(row) {
+    setTotals(row) {
         let table = document.querySelector("table");
         let footnote = table.tFoot.rows[0];
         for (let i = 0; i < row.length; i++) {
-            footnote.cells[i + 1].innerHTML = formatter.format(row[i]);
+            footnote.cells[i + 1].innerHTML = this.formatter.format(row[i]);
         }
     }
-    function print(matrix) {
+    print(matrix) {
         let tbody = document.querySelector("tbody"), i;
         for (i = 0; i < matrix.length - 1; i++) {
             let row = matrix[i];
             let tr = document.createElement("tr");
             for (let j = 0; j < row.length; j++) {
                 let cell = row[j];
-                createRows(tr, j === 0 ? cell : formatter.format(cell));
+                this.createRows(tr, j === 0 ? cell : this.formatter.format(cell));
             }
             tbody.appendChild(tr);
         }
-        setTotals(matrix[i]);
+        this.setTotals(matrix[i]);
     }
-    function compute() {
+    compute() {
         let form = document.forms[0];
         let option = form.option.selectedIndex;
         let principal = parseFloat(form.principal.value);
         let period = parseInt(form.period.value, 10);
         let interestRate = parseFloat(form.interestRate.value) / 100;
         if (principal > 0 && period > 0 && interestRate > 0) {
-            cleanTable();
+            this.cleanTable();
             let result = Systems[option].calcular(principal, period, interestRate);
-            print(result);
+            this.print(result);
         }
     }
-    function registerEvents() {
+    registerEvents() {
         let form = document.forms[0];
         let select = form.option;
         for (let i in Systems) {
@@ -57,14 +58,12 @@ function GUI() {
             option.value = i;
             select.add(option);
         }        
-        form.option.onchange = compute;
-        form.principal.onkeyup = compute;
-        form.period.onkeyup = compute;
-        form.interestRate.onkeyup = compute;
+        form.option.onchange = this.compute.bind(this);
+        form.principal.onkeyup = this.compute.bind(this);
+        form.period.onkeyup = this.compute.bind(this);
+        form.interestRate.onkeyup = this.compute.bind(this);
         form.principal.focus();
     }
-
-    return {registerEvents};
 }
-let gui = GUI();
+let gui = new GUI();
 gui.registerEvents();
